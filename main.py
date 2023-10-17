6875373e89e600afabd2b6a958ee10cee67995ef
Yonas Gebregziabher <ygebregz@bowdoin.edu>
2023-10-17 10:44:18 -0400

    print recipe + clean up

--- main.py
+++ main.py
@@ -4,11 +4,8 @@ from src.quiz import Quiz
 if __name__ == "__main__":
     quiz = Quiz()
     generator_request = quiz.run_quiz()
-    # print(f"\nRecipe name = {generator_request.get_recipe_name()}")
-    # print(f"Created multiplers =\n {generator_request.get_multipliers()}")
-    # print(f"Evaluation metric = {generator_request.get_evaluation_metric()}\n")
     recipe_gen = RecipeGenerator(generator_request.get_multipliers())
     recipe = recipe_gen.make_eval_recipe(
         generator_request.get_recipe_name(),
         generator_request.get_evaluation_metric())
-    # print(f"score = {recipe.get_eval_score()}")
+    print(recipe)
