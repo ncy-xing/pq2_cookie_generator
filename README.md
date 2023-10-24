121915754190bee657ec70745d0fd8339c955a8c
Yonas Gebregziabher <ygebregz@bowdoin.edu>
2023-10-24 11:07:37 -0400

    removed recipe files

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# pq2_cookie_generator
+# PQ2: Cookie Generation
+
+### Recipe Generation
+
+In order to guarantee that we at least get something that resembles a cookie,
+we designed our program include all the categories that make up a cookie.
+This was inspired by the PIERRE soup generation system where they divided
+the ingredients up into their respective categories.
+
+// to be continued
--- metrics.py
+++ metrics.py
@@ -4,6 +4,13 @@ easier and further individual analysis.
 """
 
 import re
+from src.recipe_generator import RecipeGenerator
+
+
+def convert_to_recipe(text):
+    lines = text.split("\n")[2:]
+    print(lines)
+
 
 # Read the entire input file
 with open('metrics/metrics.txt', 'r') as file:
@@ -12,6 +19,4 @@ with open('metrics/metrics.txt', 'r') as file:
 # Split the data into individual recipes
 recipes = re.split(r'\n\n', data)
 
-for i, recipe in enumerate(recipes):
-    with open(f"metrics/recipe{i}.txt", "w") as recipe_file:
-        recipe_file.write(recipe)
+convert_to_recipe(recipes[0])
